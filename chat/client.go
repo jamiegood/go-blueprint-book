403330001e4fc5f9c0e97096d7456a0623a30341
Jamie Good <jamie.good@hostelworld.com>
2019-10-05 18:14:37 +0100

    Finished chapter 2

--- chat/client.go
+++ chat/client.go
@@ -35,13 +35,12 @@ func (c *client) read() {
 		c.room.forward <- msg
 	}
 }
-
 func (c *client) write() {
 	defer c.socket.Close()
 	for msg := range c.send {
 		err := c.socket.WriteJSON(msg)
 		if err != nil {
-			return
+			break
 		}
 	}
 }
